3aa7f79e53b2c81b348208588086f2f53e898192
bram19 <yosaagung12@gmail.com>
2016-03-31 15:20:10 +0700

    yosa.php

---
+++ yosa.php
@@ -0,0 +1,93 @@
+<html>
+<head></head>
+<title>Table</title>
+<body bgcolor="black">
+<tr>
+<font color="black">
+<center>
+<b>YOSA AGUNG SAPUTRA</B>
+</tr>
+<tr>
+<font color="black">
+<center>
+<b>NIM : 12151470 </b>
+</center>
+<tr>
+
+<table border='1' cellpadding="0" cellspacing="0" style='colappse:0;' align='center' width='605   height="400"    '>
+<tr>
+    <td height="34" colspan="4" bgcolor="blue">
+<center>
+      	<font face="Arial">
+<font color="RED" colspan=4></div>
+          </font>		
+</font>
+    	</center>	
+	<center>
+	<font face="Arial">
+<font color="blue">
+	<div align="center"></div>
+        	</font></font>
+	<table width="602" border="0" align="left" cellpadding="0" cellspacing="0">
+        	<tr>
+          <td width="51">
+<font face="Arial">
+<font color="black"><b>No</b></font></font></td>
+          <td width="249">
+<font face="Arial">
+<font color="black"><b>Judul</b></font></font></td>
+          <td width="173"><font face="Arial">
+<font color="black"><b>downloads</b></font></font></td>
+          <td width="129"><font face="Arial">
+<font color="black"><b>grosses</b></font></font></td>
+        </tr>
+      </table>
+    </center>
+  	</td>
+  </tr>
+<?php
+	for ($i=1; $i<=5; $i++)
+	{
+	if ($i%2==1)
+		{
+		$warna="yellow";
+		echo"
+		<tr bgcolor='$warna'>
+		<td height=34><center>$i</td>
+		<td height=34 colspan=3>&nbsp
+		sule 
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+		&nbsp
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+		1,234,567
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+		&nbsp &nbsp &nbsp $123,456,789
+		</td>
+		</tr>";
+		
+		$i++;
+		}
+		else{
+		$warna="blue";
+		}
+		echo "
+		<tr bgcolor='white'>
+		<td height=33 width=43><center>$i
+		</td>
+		<td height=33 width=210> &nbspayat ayat cinta 
+		</td>
+		<td height=33 width=180><center>1,234,567</td>	
+		<td height=33><center> $123,456,789
+		</td>
+		</tr>";
+}
+?>
+<tr>
+<td height=25 colspan="4" bgcolor="orange"><b><font color="black"><center/>tiputipu.com hiiii</font></b></td></tr>
+<tr>
+ </tr>
+</table>
+</body>
+</html>
+
